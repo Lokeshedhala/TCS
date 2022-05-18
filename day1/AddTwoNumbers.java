dd6f042307471a0d65420f68875254598577a2d8
lokeshedhala <loki141479@gmail.com>
2022-05-18 13:39:41 +0000

    day1

---
+++ day1/AddTwoNumbers.java
@@ -0,0 +1,22 @@
+package com.demo;
+
+class Addition {
+	void SumOfTwoNumbers() {
+		int number1 = 10;
+		int number2 = 20;
+		int result = number1 + number2;
+		System.out.print("Sum of two numbers " + result);
+		}
+}
+
+
+		public class AddTwoNumbers {
+			public static void main(String arg[]) {
+			Addition object = new Addition();// object of this class -> AddTwoNumber
+			object.SumOfTwoNumbers();// calling from here
+			}
+		// TODO Auto-generated method stub
+
+	}
+
+
